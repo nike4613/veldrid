ffd9cd44f0c2c0f42721782dc183b8e59fd2ca06
DaNike <danike@cirr.com>
2024-09-22 21:05:12 -0500

    Begin adding types adjacent to VulkanGraphicsDevice

--- src/Veldrid/Vulkan2/VulkanGraphicsDevice.cs
+++ src/Veldrid/Vulkan2/VulkanGraphicsDevice.cs
@@ -10,6 +10,7 @@ using System.Runtime.CompilerServices;
 using TerraFX.Interop.Vulkan;
 using VkVersion = Veldrid.Vulkan.VkVersion;
 using VulkanUtil = Veldrid.Vulkan.VulkanUtil;
+using VkDeviceMemoryManager = Veldrid.Vulkan.VkDeviceMemoryManager;
 using static TerraFX.Interop.Vulkan.VkStructureType;
 using static TerraFX.Interop.Vulkan.Vulkan;
 using System.Net;
@@ -20,97 +21,159 @@ namespace Veldrid.Vulkan2
     {
         private readonly DeviceCreateState _deviceCreateState;
 
+        private readonly VkDeviceMemoryManager _memoryManager;
+        private readonly VulkanDescriptorPoolManager _descriptorPoolManager;
+        private readonly object _queueLock = new();
+
+        private readonly VkCommandPool _commandPool;
+
         // optional functions
 
         // debug marker ext
-        private readonly unsafe delegate* unmanaged<VkDevice, VkDebugMarkerObjectNameInfoEXT*, VkResult> vkDebugMarkerSetObjectNameEXT;
-        private readonly unsafe delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void> vkCmdDebugMarkerBeginEXT;
-        private readonly unsafe delegate* unmanaged<VkCommandBuffer, void> vkCmdDebugMarkerEndEXT;
-        private readonly unsafe delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void> vkCmdDebugMarkerInsertEXT;
+        internal readonly unsafe delegate* unmanaged<VkDevice, VkDebugMarkerObjectNameInfoEXT*, VkResult> vkDebugMarkerSetObjectNameEXT;
+        internal readonly unsafe delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void> vkCmdDebugMarkerBeginEXT;
+        internal readonly unsafe delegate* unmanaged<VkCommandBuffer, void> vkCmdDebugMarkerEndEXT;
+        internal readonly unsafe delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void> vkCmdDebugMarkerInsertEXT;
         // dedicated allocation and memreq2
-        private readonly unsafe delegate* unmanaged<VkDevice, VkBufferMemoryRequirementsInfo2*, VkMemoryRequirements2*, void> vkGetBufferMemoryRequirements2;
-        private readonly unsafe delegate* unmanaged<VkDevice, VkImageMemoryRequirementsInfo2*, VkMemoryRequirements2*, void> vkGetImageMemoryRequirements2;
+        internal readonly unsafe delegate* unmanaged<VkDevice, VkBufferMemoryRequirementsInfo2*, VkMemoryRequirements2*, void> vkGetBufferMemoryRequirements2;
+        internal readonly unsafe delegate* unmanaged<VkDevice, VkImageMemoryRequirementsInfo2*, VkMemoryRequirements2*, void> vkGetImageMemoryRequirements2;
 
         public string? DriverName { get; }
         public string? DriverInfo { get; }
 
         private unsafe VulkanGraphicsDevice(ref DeviceCreateState deviceCreateState)
         {
-            // once we adopt the DCS, default-out the source because the caller will try to free the handles (which we now own)
-            _deviceCreateState = deviceCreateState;
-            deviceCreateState = default;
-
-            // Populate knowns based on the fact that this is a Vulkan implementation
-            BackendType = GraphicsBackend.Vulkan;
-            IsUvOriginTopLeft = true;
-            IsDepthRangeZeroToOne = true;
-            IsClipSpaceYInverted = true;
-            ApiVersion = new(
-                (int)_deviceCreateState.ApiVersion.Major,
-                (int)_deviceCreateState.ApiVersion.Minor,
-                (int)_deviceCreateState.ApiVersion.Patch, 0);
-
-            // Then stuff out of the physical device properties
-            UniformBufferMinOffsetAlignment = (uint)_deviceCreateState.PhysicalDeviceProperties.limits.minUniformBufferOffsetAlignment;
-            StructuredBufferMinOffsetAlignment = (uint)_deviceCreateState.PhysicalDeviceProperties.limits.minStorageBufferOffsetAlignment;
-            DeviceName = Util.GetString(_deviceCreateState.PhysicalDeviceProperties.deviceName);
-
-            // Then driver properties (if available)
-            if (_deviceCreateState.HasDriverPropertiesExt)
+            try
             {
-                var vkGetPhysicalDeviceProperties2 =
-                    (delegate* unmanaged<VkPhysicalDevice, void*, void>)
-                    GetInstanceProcAddr("vkGetPhysicalDeviceProperties2"u8, "vkGetPhysicalDeviceProperties2KHR"u8);
+                // once we adopt the DCS, default-out the source because the caller will try to free the handles (which we now own)
+                _deviceCreateState = deviceCreateState;
+                deviceCreateState = default;
 
-                if (vkGetPhysicalDeviceProperties2 is not null)
+                // Populate knowns based on the fact that this is a Vulkan implementation
+                BackendType = GraphicsBackend.Vulkan;
+                IsUvOriginTopLeft = true;
+                IsDepthRangeZeroToOne = true;
+                IsClipSpaceYInverted = true;
+                ApiVersion = new(
+                    (int)_deviceCreateState.ApiVersion.Major,
+                    (int)_deviceCreateState.ApiVersion.Minor,
+                    (int)_deviceCreateState.ApiVersion.Patch, 0);
+
+                // Then stuff out of the physical device properties
+                UniformBufferMinOffsetAlignment = (uint)_deviceCreateState.PhysicalDeviceProperties.limits.minUniformBufferOffsetAlignment;
+                StructuredBufferMinOffsetAlignment = (uint)_deviceCreateState.PhysicalDeviceProperties.limits.minStorageBufferOffsetAlignment;
+                DeviceName = Util.GetString(_deviceCreateState.PhysicalDeviceProperties.deviceName);
+                VendorName = $"id:{_deviceCreateState.PhysicalDeviceProperties.vendorID:x8}";
+
+                // Then driver properties (if available)
+                if (_deviceCreateState.HasDriverPropertiesExt)
                 {
-                    var driverProps = new VkPhysicalDeviceDriverProperties()
-                    {
-                        sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DRIVER_PROPERTIES,
-                    };
-                    var deviceProps = new VkPhysicalDeviceProperties2()
-                    {
-                        sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
-                        pNext = &driverProps,
-                    };
-                    vkGetPhysicalDeviceProperties2(_deviceCreateState.PhysicalDevice, &deviceProps);
+                    var vkGetPhysicalDeviceProperties2 =
+                        (delegate* unmanaged<VkPhysicalDevice, void*, void>)
+                        GetInstanceProcAddr("vkGetPhysicalDeviceProperties2"u8, "vkGetPhysicalDeviceProperties2KHR"u8);
 
-                    DriverName = Util.GetString(driverProps.driverName);
-                    DriverInfo = Util.GetString(driverProps.driverInfo);
+                    if (vkGetPhysicalDeviceProperties2 is not null)
+                    {
+                        var driverProps = new VkPhysicalDeviceDriverProperties()
+                        {
+                            sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DRIVER_PROPERTIES,
+                        };
+                        var deviceProps = new VkPhysicalDeviceProperties2()
+                        {
+                            sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
+                            pNext = &driverProps,
+                        };
+                        vkGetPhysicalDeviceProperties2(_deviceCreateState.PhysicalDevice, &deviceProps);
 
-                    ApiVersion = new(
-                        driverProps.conformanceVersion.major,
-                        driverProps.conformanceVersion.minor,
-                        driverProps.conformanceVersion.subminor,
-                        driverProps.conformanceVersion.patch);
+                        DriverName = Util.GetString(driverProps.driverName);
+                        DriverInfo = Util.GetString(driverProps.driverInfo);
+
+                        ApiVersion = new(
+                            driverProps.conformanceVersion.major,
+                            driverProps.conformanceVersion.minor,
+                            driverProps.conformanceVersion.subminor,
+                            driverProps.conformanceVersion.patch);
+                    }
                 }
-            }
 
-            // Then several optional extension functions
-            if (_deviceCreateState.HasDebugMarkerExt)
+                // Then several optional extension functions
+                if (_deviceCreateState.HasDebugMarkerExt)
+                {
+                    vkDebugMarkerSetObjectNameEXT =
+                        (delegate* unmanaged<VkDevice, VkDebugMarkerObjectNameInfoEXT*, VkResult>)GetInstanceProcAddr("vkDebugMarkerSetObjectNameEXT"u8);
+                    vkCmdDebugMarkerBeginEXT =
+                        (delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void>)GetInstanceProcAddr("vkCmdDebugMarkerBeginEXT"u8);
+                    vkCmdDebugMarkerEndEXT =
+                        (delegate* unmanaged<VkCommandBuffer, void>)GetInstanceProcAddr("vkCmdDebugMarkerEndEXT"u8);
+                    vkCmdDebugMarkerInsertEXT =
+                        (delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void>)GetInstanceProcAddr("vkCmdDebugMarkerInsertEXT"u8);
+                }
+
+                if (_deviceCreateState.HasDedicatedAllocationExt && _deviceCreateState.HasMemReqs2Ext)
+                {
+                    vkGetBufferMemoryRequirements2 =
+                        (delegate* unmanaged<VkDevice, VkBufferMemoryRequirementsInfo2*, VkMemoryRequirements2*, void>)
+                        GetDeviceProcAddr("vkGetBufferMemoryRequirements2"u8, "vkGetBufferMemoryRequirements2KHR"u8);
+                    vkGetImageMemoryRequirements2 =
+                        (delegate* unmanaged<VkDevice, VkImageMemoryRequirementsInfo2*, VkMemoryRequirements2*, void>)
+                        GetDeviceProcAddr("vkGetImageMemoryRequirements2"u8, "vkGetImageMemoryRequirements2KHR"u8);
+                }
+
+                // Create other bits and pieces
+                _memoryManager = new(
+                    _deviceCreateState.Device,
+                    _deviceCreateState.PhysicalDevice,
+                    _deviceCreateState.PhysicalDeviceProperties.limits.bufferImageGranularity,
+                    chunkGranularity: 1024);
+
+                Features = new(
+                    computeShader: _deviceCreateState.QueueFamilyInfo.MainComputeFamilyIdx >= 0,
+                    geometryShader: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.geometryShader,
+                    tessellationShaders: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.tessellationShader,
+                    multipleViewports: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.multiViewport,
+                    samplerLodBias: true,
+                    drawBaseVertex: true,
+                    drawBaseInstance: true,
+                    drawIndirect: true,
+                    drawIndirectBaseInstance: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.drawIndirectFirstInstance,
+                    fillModeWireframe: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.fillModeNonSolid,
+                    samplerAnisotropy: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.samplerAnisotropy,
+                    depthClipDisable: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.depthClamp,
+                    texture1D: true,
+                    independentBlend: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.independentBlend,
+                    structuredBuffer: true,
+                    subsetTextureView: true,
+                    commandListDebugMarkers: _deviceCreateState.HasDebugMarkerExt,
+                    bufferRangeBinding: true,
+                    shaderFloat64: (VkBool32)_deviceCreateState.PhysicalDeviceFeatures.shaderFloat64);
+
+                ResourceFactory = new VulkanResourceFactory(this);
+                _descriptorPoolManager = new(this);
+
+                _commandPool = CreateCommandPool(this);
+                static VkCommandPool CreateCommandPool(VulkanGraphicsDevice device)
+                {
+                    var commandPoolCreateInfo = new VkCommandPoolCreateInfo()
+                    {
+                        sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+                        flags = VkCommandPoolCreateFlags.VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+                        queueFamilyIndex = (uint)device._deviceCreateState.QueueFamilyInfo.MainGraphicsFamilyIdx,
+                    };
+                    VkCommandPool commandPool;
+                    VulkanUtil.CheckResult(vkCreateCommandPool(device._deviceCreateState.Device, &commandPoolCreateInfo, null, &commandPool));
+                    return commandPool;
+                }
+
+                // TODO: MainSwapchain
+
+                PostDeviceCreated();
+            }
+            catch
             {
-                vkDebugMarkerSetObjectNameEXT =
-                    (delegate* unmanaged<VkDevice, VkDebugMarkerObjectNameInfoEXT*, VkResult>)GetInstanceProcAddr("vkDebugMarkerSetObjectNameEXT"u8);
-                vkCmdDebugMarkerBeginEXT =
-                    (delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void>)GetInstanceProcAddr("vkCmdDebugMarkerBeginEXT"u8);
-                vkCmdDebugMarkerEndEXT =
-                    (delegate* unmanaged<VkCommandBuffer, void>)GetInstanceProcAddr("vkCmdDebugMarkerEndEXT"u8);
-                vkCmdDebugMarkerInsertEXT =
-                    (delegate* unmanaged<VkCommandBuffer, VkDebugMarkerMarkerInfoEXT*, void>)GetInstanceProcAddr("vkCmdDebugMarkerInsertEXT"u8);
+                // eagerly dispose if we threw here
+                DisposeDirectOwned();
+                throw;
             }
-
-            if (_deviceCreateState.HasDedicatedAllocationExt && _deviceCreateState.HasMemReqs2Ext)
-            {
-                vkGetBufferMemoryRequirements2 =
-                    (delegate* unmanaged<VkDevice, VkBufferMemoryRequirementsInfo2*, VkMemoryRequirements2*, void>)
-                    GetDeviceProcAddr("vkGetBufferMemoryRequirements2"u8, "vkGetBufferMemoryRequirements2KHR"u8);
-                vkGetImageMemoryRequirements2 =
-                    (delegate* unmanaged<VkDevice, VkImageMemoryRequirementsInfo2*, VkMemoryRequirements2*, void>)
-                    GetDeviceProcAddr("vkGetImageMemoryRequirements2"u8, "vkGetImageMemoryRequirements2KHR"u8);
-            }
-
-            throw new NotImplementedException();
-
         }
 
         private bool _disposed;
@@ -119,12 +182,32 @@ namespace Veldrid.Vulkan2
         {
             base.Dispose(disposing);
 
+            DisposeDirectOwned();
+        }
+
+        private unsafe void DisposeDirectOwned()
+        {
             if (_disposed) return;
             _disposed = true;
 
             // TODO: destroy all other associated information
 
             var dcs = _deviceCreateState;
+
+            if (_commandPool != VkCommandPool.NULL)
+            {
+                vkDestroyCommandPool(dcs.Device, _commandPool, null);
+            }
+
+            if (_descriptorPoolManager is { } poolManager)
+            {
+                poolManager.DestroyAll();
+            }
+
+            if (_memoryManager is { } memoryManager)
+            {
+                memoryManager.Dispose();
+            }
 
             if (dcs.Device != VkDevice.NULL)
             {
@@ -149,6 +232,8 @@ namespace Veldrid.Vulkan2
                 vkDestroyInstance(dcs.Instance, null);
             }
         }
+
+        public VkDevice Device => _deviceCreateState.Device;
 
         public override TextureSampleCount GetSampleCountLimit(PixelFormat format, bool depthFormat)
         {
@@ -207,7 +292,12 @@ namespace Veldrid.Vulkan2
 
         private protected override void WaitForIdleCore()
         {
-            throw new NotImplementedException();
+            lock (_queueLock)
+            {
+                vkQueueWaitIdle(_deviceCreateState.MainQueue);
+            }
+
+            // TODO: CheckSubmittedFences()
         }
     }
 }
---
+++ src/Veldrid/Vulkan2/VulkanResourceFactory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veldrid.Vulkan2
+{
+    internal sealed class VulkanResourceFactory : ResourceFactory
+    {
+        private readonly VulkanGraphicsDevice _gd;
+
+        public VulkanResourceFactory(VulkanGraphicsDevice gd) : base(gd.Features)
+        {
+            _gd = gd;
+        }
+
+        public override GraphicsBackend BackendType => throw new NotImplementedException();
+
+        public override DeviceBuffer CreateBuffer(in BufferDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CommandList CreateCommandList(in CommandListDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Pipeline CreateComputePipeline(in ComputePipelineDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Fence CreateFence(bool signaled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Framebuffer CreateFramebuffer(in FramebufferDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Pipeline CreateGraphicsPipeline(in GraphicsPipelineDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ResourceLayout CreateResourceLayout(in ResourceLayoutDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ResourceSet CreateResourceSet(in ResourceSetDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Sampler CreateSampler(in SamplerDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Shader CreateShader(in ShaderDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Swapchain CreateSwapchain(in SwapchainDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Texture CreateTexture(in TextureDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Texture CreateTexture(ulong nativeTexture, in TextureDescription description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TextureView CreateTextureView(in TextureViewDescription description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
